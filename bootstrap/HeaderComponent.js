a17b2e9811f0c51302be6fc76000b33561abe708
Rahul__Chaubey <87417016+Rahul-Chaubey@users.noreply.github.com>
2021-11-30 16:51:59 +0530

    Add files via upload

---
+++ bootstrap/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+
+export default class HeaderComponent extends Component {
+
+
+render () {
+  return (
+      <>
+      <h1>IN HEADER</h1>
+      </>
+  )
+}
+}
---
+++ bootstrap/ToDoComponent.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+
+export default class ToDoComponent extends Component {
+    render() {
+        return(
+            <h1>I am in ToDoComponent</h1>
+        )
+    }
+}
